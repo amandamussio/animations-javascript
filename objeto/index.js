8bfe09caa1dcbe1585defc6e7fdb7a4705179921
Amanda Macedo Mussio <amandamussio@Amandas-MacBook-Air.local>
2018-11-21 21:42:13 -0200

    DOM

---
+++ objeto/index.js
@@ -0,0 +1,16 @@
+// var carro = 'March';
+// var montadora = 'Nissan';
+// var ano = 2019;
+// var foto = 'https://carros2019.com.br/wp-content/uploads/2018/04/Screenshot_6.png';
+// console.log(carro, montadora, ano, foto)
+
+var carro = {
+    modelo: 'March',
+    montadora: 'Nissan',
+    ano: 2019,
+    foto: 'https://carros2019.com.br/wp-content/uploads/2018/04/Screenshot_6.png',
+    vendido: false
+}
+
+console.log('Modelo ', carro.modelo);
+console.log('Montadora ', carro.montadora);
