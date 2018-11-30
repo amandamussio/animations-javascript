cfbccb9c1b03a9e5d5c6f0667e686bc4cab992d5
amandamussio <mussioamanda@gmail.com>
2018-11-30 06:39:45 -0200

    transformado em function

--- DOM/eventos/index.js
+++ DOM/eventos/index.js
@@ -2,23 +2,31 @@ var buttonBlack = document.getElementById('button-black');
 var car = document.getElementById('image-car');
 
 function captureColor(event) {
-    // console.log(event)
-    const element = event.target;
-    let color = element.getAttribute("data-color")
-    console.log(color)
+    const element = event.target; //captura o elemento clicado
+    let color = element.getAttribute("data-color") //captura um determinado atributo
+    // console.log(color)
     switch(color) {
         case 'black': 
-            car.setAttribute('src','./images/carro-preto.png');
+            // car.setAttribute('src','./images/carro-preto.png');
+            changeCar('preto')
             break;
         case 'red':
-            car.setAttribute('src','./images/carro-vermelho.png');
+            // car.setAttribute('src','./images/carro-branco.png');
+            changeCar('vermelho')
             break;
         case 'white': 
-            car.setAttribute('src','./images/carro-branco.png');  
+            // car.setAttribute('src','./images/carro-branco.png');  
+            changeCar('branco')
             break;
         case 'silver':   
-            car.setAttribute('src','./images/carro-prata.png');
+            // car.setAttribute('src','./images/carro-prata.png');
+            changeCar('prata')
             break;
         default:           
     }
+}
+
+function changeCar(nomeImagem) {
+    // car.setAttribute('src','./images/carro-'+nomeImagem+ '.png');
+    car.setAttribute('src',`./images/carro-${nomeImagem}.png`);
 }
